7c9442bf7382e3eafbe6d77bd2b2d7adce642a8f
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-03-25 20:15:45 -0300

    Update testgrid.py

--- project/testgrid.py
+++ project/testgrid.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
 root = tk.Tk()
-canvas = tk.Canvas(root, width = 500, height = 500)
+canvas = tk.Canvas(root, width=500, height=500)
 canvas.pack()
 
-for i in range(0, 500, 50):
-    canvas.create_line(i, 0, i, 500, fill="black")
-    canvas.create_line(0, i, 500, i, fill="black")
+cell_size = 50  # Tamanho das células
+
+for i in range(0, 500, cell_size):
+    for j in range(0, 500, cell_size):
+        # color = "#ADD8E6" if (i // cell_size + j // cell_size) % 2 == 0 else "#FFFFFF"
+        canvas.create_rectangle(i, j, i + cell_size, j + cell_size, fill="#FFFFFF", outline="black")
 
 root.mainloop()
