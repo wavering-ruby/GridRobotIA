e20d896f16d91c5831b20e1dd71d751f9d7b4a6b
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-05-12 09:45:15 -0300

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# Iniciando com algumas configurações locais simples, como versão.
+application_datails = {
+  "version": "1.0.0",
+  "creators": ["Mateus Gabriel Mendes de Paula", "Caio Viana de Jesus"]
+}
