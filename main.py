eaf45c4296f522b889099bfafdb8a3f42f096465
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-07-31 21:51:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from ui.user_interface import UserInterface
 if __name__ == "__main__":
     app = UserInterface()
     app.run()
+
+# to create the executable: python -m PyInstaller --noconfirm --clean --windowed --add-data "assets;assets" --add-data "core;core" --add-data "ui;ui" main.py
