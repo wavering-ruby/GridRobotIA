baa0c7c889db59d4f3a7aa441ff5b2281c77c2cb
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-07-31 21:31:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GridBot-AI",
-    version = "1.0.0",
+    version = "Demo 2.4.0",
+    description = "A grid-based AI path finder visualization tool",
     packages = find_packages(),
     package_data = {"GridBot_AI": ["assets/*.png"]},  # Inclui arquivos estáticos
+    install_requires = [
+        "pygame", 
+        "pygame_gui",
+        "numpy"
+    ]
 )
